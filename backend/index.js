fea9955c873f627941d6e71cdc3dd3eab71aedfe
Rohit Verma <Rohit.verma272727@gmail.com>
2024-08-23 10:58:55 +0000

    feat: Created express server and connected to mysql database

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+import express from 'express'
+
+const app = express();
+dotenv.config();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+export const db = mysql.createConnection({
+    host: `${process.env.HOST}`,
+    user: `${process.env.USER_NAME}`,
+    password: `${process.env.PASSWORD}`,
+    database: `${process.env.DATABASE}` 
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to mysql database database');
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Listening on PORT: ${process.env.PORT}...`);
+    })
+});
+
+
